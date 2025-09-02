2f45ae061a2c6e5b94ae93e4605cdae1e5ca4d50
eshu786 <87356401+eshu786@users.noreply.github.com>
2025-09-02 18:03:59 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "humynex-terraform-state"   # your S3 bucket
+    key            = "eks/terraform.tfstate"     # path inside the bucket
+    region         = "ap-south-1"               # your region
+    dynamodb_table = "humynex-terraform-locks"  # DynamoDB table for state locking
+    encrypt        = true
+  }
+}
